84951e7e61c83c5b87fb669b22510d182e3f87f8
miantsaiarilanja <miantsaiarilanja@gmail.com>
2023-07-04 14:47:30 +0300

    Premier commit

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Routes, Route, Router} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App;
+
+
